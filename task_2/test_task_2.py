001d6bf8fe1a5c3a97c9b91c443f46ff41af8412
Ilia-Abrosimov <abrosimof93@gmail.com>
2021-12-04 15:01:43 +0300

    Task_2 update

--- task_2/test_task_2.py
+++ task_2/test_task_2.py
@@ -1,18 +1,44 @@
 import pytest
-try:
-    import author
-except ImportError:
-    assert False, 'Не найден класс Contact'
+from author import mike as author_mike
+from author import vlad as author_vlad
+from precode import Contact, mike, vlad
 
 
-class TestClassVariable:
-    init_records = [{'name': "Test", 'phone': '123', "birthday": "03.12.2021", "address": "Moscow"}]
+class TestClass:
 
-    @pytest.mark.parametrize("kwargs", init_records)
-    def test_class_name(self, kwargs):
-        assert hasattr(author, "Contact")
-        result = author.Contact(**kwargs)
-        assert hasattr(result, "name")
-        assert hasattr(result, "phone")
-        assert hasattr(result, "birthday")
-        assert hasattr(result, "address")
+    def test_class_exist(self):
+        try:
+            import precode
+        except ImportError:
+            assert False, 'Не найден модуль precode'
+        assert hasattr(precode, "Contact"), "Не найден класс Contact"
+
+    def test_show_contact(self):
+        assert "show_contact" in dir(Contact), "У класса Contact отсутствует метод show_contact"
+
+    def test_mike_attribute(self):
+        try:
+            from precode import mike
+        except ImportError:
+            assert False, 'Отсутствует объект mike'
+        assert mike.name == author_mike.name, "Неверное значение свойства name у объекта mike"
+        assert mike.phone == author_mike.phone, "Неверное значение свойства phone у объекта mike"
+        assert mike.birthday == author_mike.birthday, "Неверное значение свойства birthday у объекта mike"
+        assert mike.address == author_mike.address, "Неверное значение свойства address у объекта mike"
+
+    def test_vlad_attribute(self):
+        try:
+            from precode import vlad
+        except ImportError:
+            assert False, 'Отсутствует объект mike'
+        assert vlad.name == author_vlad.name, "Неверное значение свойства name у объекта vlad"
+        assert vlad.phone == author_vlad.phone, "Неверное значение свойства phone у объекта vlad"
+        assert vlad.birthday == author_vlad.birthday, "Неверное значение свойства birthday у объекта vlad"
+        assert vlad.address == author_vlad.address, "Неверное значение свойства address у объекта vlad"
+
+    def test_output(self, task_2_output):
+        mike_show_contact = "Михаил Булгаков — адрес: Россия, Москва, Большая Пироговская, дом 35б, кв. 6, телефон: 2-03-27, день рождения: 15.05.1891"
+        vlad_show_contact = "Владимир Маяковский — адрес: Россия, Москва, Лубянский проезд, д. 3, кв. 12, телефон: 73-88, день рождения: 19.07.1893"
+        assert mike_show_contact in task_2_output, "Обратитесь к методу show_contact() объекта mike"
+        assert vlad_show_contact in task_2_output, "Обратитесь к методу show_contact() объекта vlad"
+
