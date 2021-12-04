06bb4d2bb9d251ed4a314e3fad3103cdb8a67b9f
Ilia-Abrosimov <abrosimof93@gmail.com>
2021-12-04 17:35:51 +0300

    add msg error

--- task_2/test_task_2.py
+++ task_2/test_task_2.py
@@ -1,7 +1,12 @@
 import pytest
 from author import mike as author_mike
 from author import vlad as author_vlad
-from precode import Contact, mike, vlad
+from precode import Contact
+import precode
+
+
+def send_msg_value_error(property, object):
+    return f"Неверное значение свойства {property} у объекта {object}"
 
 
 class TestClass:
@@ -14,27 +19,27 @@ class TestClass:
         assert hasattr(precode, "Contact"), "Не найден класс Contact"
 
     def test_show_contact(self):
-        assert "show_contact" in dir(Contact), "У класса Contact отсутствует метод show_contact"
+        assert hasattr(Contact, "show_contact"), "У класса Contact отсутствует метод show_contact"
 
     def test_mike_attribute(self):
         try:
             from precode import mike
         except ImportError:
             assert False, 'Отсутствует объект mike'
-        assert mike.name == author_mike.name, "Неверное значение свойства name у объекта mike"
-        assert mike.phone == author_mike.phone, "Неверное значение свойства phone у объекта mike"
-        assert mike.birthday == author_mike.birthday, "Неверное значение свойства birthday у объекта mike"
-        assert mike.address == author_mike.address, "Неверное значение свойства address у объекта mike"
+        assert mike.name == author_mike.name, send_msg_value_error("name", "mike")
+        assert mike.phone == author_mike.phone, send_msg_value_error("phone", "mike")
+        assert mike.birthday == author_mike.birthday, send_msg_value_error("birthday", "mike")
+        assert mike.address == author_mike.address, send_msg_value_error("address", "mike")
 
     def test_vlad_attribute(self):
         try:
             from precode import vlad
         except ImportError:
             assert False, 'Отсутствует объект mike'
-        assert vlad.name == author_vlad.name, "Неверное значение свойства name у объекта vlad"
-        assert vlad.phone == author_vlad.phone, "Неверное значение свойства phone у объекта vlad"
-        assert vlad.birthday == author_vlad.birthday, "Неверное значение свойства birthday у объекта vlad"
-        assert vlad.address == author_vlad.address, "Неверное значение свойства address у объекта vlad"
+        assert vlad.name == author_vlad.name, send_msg_value_error("name", "vlad")
+        assert vlad.phone == author_vlad.phone, send_msg_value_error("phone", "vlad")
+        assert vlad.birthday == author_vlad.birthday, send_msg_value_error("birthday", "vlad")
+        assert vlad.address == author_vlad.address, send_msg_value_error("address", "vlad")
 
     def test_output(self, task_2_output):
         mike_show_contact = "Михаил Булгаков — адрес: Россия, Москва, Большая Пироговская, дом 35б, кв. 6, телефон: 2-03-27, день рождения: 15.05.1891"
@@ -42,3 +47,5 @@ class TestClass:
         assert mike_show_contact in task_2_output, "Обратитесь к методу show_contact() объекта mike"
         assert vlad_show_contact in task_2_output, "Обратитесь к методу show_contact() объекта vlad"
 
+    def test_print_contact(self):
+        assert hasattr(precode, "print_contact"), "Функция print_contact не удалена"
